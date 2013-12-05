ddd022c4b1747308602ac14d7af878ed57ff3697
Mahmoud Ismail <maism@kth.se>
2013-12-05 16:40:46 +0100

    add block token

--- src/main/java/se/sics/hop/metadata/persistence/ndb/NdbStorageFactory.java
+++ src/main/java/se/sics/hop/metadata/persistence/ndb/NdbStorageFactory.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Properties;
 import se.sics.hop.metadata.persistence.DALStorageFactory;
 import se.sics.hop.metadata.persistence.StorageConnector;
+import se.sics.hop.metadata.persistence.dal.BlockTokenKeyDataAccess;
 import se.sics.hop.metadata.persistence.dal.CorruptReplicaDataAccess;
 import se.sics.hop.metadata.persistence.dal.EntityDataAccess;
 import se.sics.hop.metadata.persistence.dal.ExcessReplicaDataAccess;
@@ -15,6 +16,7 @@ import se.sics.hop.metadata.persistence.dal.LeasePathDataAccess;
 import se.sics.hop.metadata.persistence.dal.ReplicaDataAccess;
 import se.sics.hop.metadata.persistence.dal.UnderReplicatedBlockDataAccess;
 import se.sics.hop.metadata.persistence.dal.VariableDataAccess;
+import se.sics.hop.metadata.persistence.ndb.dalimpl.BlockTokenKeyClusterj;
 import se.sics.hop.metadata.persistence.ndb.dalimpl.CorruptReplicaClusterj;
 import se.sics.hop.metadata.persistence.ndb.dalimpl.ExcessReplicaClusterj;
 import se.sics.hop.metadata.persistence.ndb.dalimpl.InvalidatedBlockClusterj;
@@ -50,6 +52,7 @@ public class NdbStorageFactory implements DALStorageFactory {
     dataAccessMap.put(InvalidateBlockDataAccess.class, new InvalidatedBlockClusterj());
     dataAccessMap.put(UnderReplicatedBlockDataAccess.class, new UnderReplicatedBlockClusterj());
     dataAccessMap.put(VariableDataAccess.class, new VariableClusterj());
+    dataAccessMap.put(BlockTokenKeyDataAccess.class, new BlockTokenKeyClusterj());
   }
 
   public StorageConnector getConnector() {
