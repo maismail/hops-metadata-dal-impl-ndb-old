c7f5d81a8e0dfc928707d7f0847a8e18235a77ca
Salman Niazi <smkniazi@kth.se>
2014-08-27 12:35:09 +0200

    changed max transactions to 1024

--- src/main/java/se/sics/hop/metadata/ndb/dalimpl/hdfs/LeasePathClusterj.java
+++ src/main/java/se/sics/hop/metadata/ndb/dalimpl/hdfs/LeasePathClusterj.java
@@ -25,154 +25,156 @@ import se.sics.hop.metadata.hdfs.tabledef.LeasePathTableDef;
  */
 public class LeasePathClusterj implements LeasePathTableDef, LeasePathDataAccess<HopLeasePath> {
 
-  @PersistenceCapable(table = TABLE_NAME)
-  public interface LeasePathsDTO {
+    @PersistenceCapable(table = TABLE_NAME)
+    public interface LeasePathsDTO {
 
-    @Column(name = HOLDER_ID)
-    int getHolderId();
-    void setHolderId(int holder_id);
+        @Column(name = HOLDER_ID)
+        int getHolderId();
+        void setHolderId(int holder_id);
 
-    @PrimaryKey
-    @Column(name = PATH)
-    String getPath();
-    void setPath(String path);
-    
-    @PrimaryKey
-    @Column(name = PART_KEY)
-    int getPartKey();
-    void setPartKey(int partKey);
-  }
-  private ClusterjConnector connector = ClusterjConnector.getInstance();
+        @PrimaryKey
+        @Column(name = PATH)
+        String getPath();
 
-  @Override
-  public void prepare(Collection<HopLeasePath> removed, Collection<HopLeasePath> newed, Collection<HopLeasePath> modified) throws StorageException {
-    try {
-      List<LeasePathsDTO> changes = new ArrayList<LeasePathsDTO>();
-      List<LeasePathsDTO> deletions = new ArrayList<LeasePathsDTO>();
-      Session session = connector.obtainSession();
-      for (HopLeasePath lp : newed) {
-        LeasePathsDTO lTable = session.newInstance(LeasePathsDTO.class);
-        createPersistableLeasePathInstance(lp, lTable);
-        changes.add(lTable);
-      }
+        void setPath(String path);
 
-      for (HopLeasePath lp : modified) {
-        LeasePathsDTO lTable = session.newInstance(LeasePathsDTO.class);
-        createPersistableLeasePathInstance(lp, lTable);
-        changes.add(lTable);
-      }
+        @PrimaryKey
+        @Column(name = PART_KEY)
+        int getPartKey();
 
-      for (HopLeasePath lp : removed) {
+        void setPartKey(int partKey);
+    }
+    private ClusterjConnector connector = ClusterjConnector.getInstance();
+
+    @Override
+    public void prepare(Collection<HopLeasePath> removed, Collection<HopLeasePath> newed, Collection<HopLeasePath> modified) throws StorageException {
+        try {
+            List<LeasePathsDTO> changes = new ArrayList<LeasePathsDTO>();
+            List<LeasePathsDTO> deletions = new ArrayList<LeasePathsDTO>();
+            Session session = connector.obtainSession();
+            for (HopLeasePath lp : newed) {
+                LeasePathsDTO lTable = session.newInstance(LeasePathsDTO.class);
+                createPersistableLeasePathInstance(lp, lTable);
+                changes.add(lTable);
+            }
+
+            for (HopLeasePath lp : modified) {
+                LeasePathsDTO lTable = session.newInstance(LeasePathsDTO.class);
+                createPersistableLeasePathInstance(lp, lTable);
+                changes.add(lTable);
+            }
+
+            for (HopLeasePath lp : removed) {
         Object[] key = new Object[2];
         key[0] = lp.getPath();
         key[1] = PART_KEY_VAL;
-        LeasePathsDTO lTable = session.newInstance(LeasePathsDTO.class, key);
-        deletions.add(lTable);
-      }
-      session.deletePersistentAll(deletions);
-      session.savePersistentAll(changes);
-    } catch (Exception e) {
-      throw new StorageException(e);
+                LeasePathsDTO lTable = session.newInstance(LeasePathsDTO.class, key);
+                deletions.add(lTable);
+            }
+            session.deletePersistentAll(deletions);
+            session.savePersistentAll(changes);
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
     }
-  }
 
-  @Override
-  public Collection<HopLeasePath> findByHolderId(int holderId) throws StorageException {
-    try {
-      Session session = connector.obtainSession();
-      QueryBuilder qb = session.getQueryBuilder();
-      QueryDomainType<LeasePathsDTO> dobj = qb.createQueryDefinition(LeasePathsDTO.class);
-      Predicate pred1 = dobj.get("holderId").equal(dobj.param("param1"));
-      Predicate pred2 = dobj.get("partKey").equal(dobj.param("param2"));
-      dobj.where(pred1);
-      Query<LeasePathsDTO> query = session.createQuery(dobj);
-      query.setParameter("param1", holderId);
-      query.setParameter("param2", PART_KEY_VAL);
-      return createList(query.getResultList());
-    } catch (Exception e) {
-      throw new StorageException(e);
+    @Override
+    public Collection<HopLeasePath> findByHolderId(int holderId) throws StorageException {
+        try {
+            Session session = connector.obtainSession();
+            QueryBuilder qb = session.getQueryBuilder();
+            QueryDomainType<LeasePathsDTO> dobj = qb.createQueryDefinition(LeasePathsDTO.class);
+            Predicate pred1 = dobj.get("holderId").equal(dobj.param("param1"));
+            Predicate pred2 = dobj.get("partKey").equal(dobj.param("param2"));
+            dobj.where(pred1);
+            Query<LeasePathsDTO> query = session.createQuery(dobj);
+            query.setParameter("param1", holderId);
+            query.setParameter("param2", PART_KEY_VAL);
+            return createList(query.getResultList());
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
     }
-  }
 
-  @Override
-  public HopLeasePath findByPKey(String path) throws StorageException {
-    try {
+    @Override
+    public HopLeasePath findByPKey(String path) throws StorageException {
+        try {
       Object[] key = new Object[2];
       key[0] = path;
       key[1] = PART_KEY_VAL;
-      Session session = connector.obtainSession();
+            Session session = connector.obtainSession();
       LeasePathsDTO lPTable = session.find(LeasePathsDTO.class, key);
       HopLeasePath lPath = null;
       if (lPTable != null) {
         lPath = createLeasePath(lPTable);
-      }
+                }
       return lPath;
-    } catch (Exception e) {
-      throw new StorageException(e);
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
     }
-  }
 
-  @Override
-  public Collection<HopLeasePath> findByPrefix(String prefix) throws StorageException {
-    try {
-      Session session = connector.obtainSession();
-      QueryBuilder qb = session.getQueryBuilder();
-      QueryDomainType dobj = qb.createQueryDefinition(LeasePathsDTO.class);
-      PredicateOperand propertyPredicate = dobj.get("path");
-      String param = "prefix";
-      PredicateOperand propertyLimit = dobj.param(param);
-      Predicate like = propertyPredicate.like(propertyLimit).and(dobj.get("partKey").equal(dobj.param("partKeyParam")));
-      dobj.where(like);
-      Query query = session.createQuery(dobj);
-      query.setParameter(param, prefix + "%");
-      query.setParameter("partKeyParam", PART_KEY_VAL);
-      return createList(query.getResultList());
-    } catch (Exception e) {
-      throw new StorageException(e);
+    @Override
+    public Collection<HopLeasePath> findByPrefix(String prefix) throws StorageException {
+        try {
+            Session session = connector.obtainSession();
+            QueryBuilder qb = session.getQueryBuilder();
+            QueryDomainType dobj = qb.createQueryDefinition(LeasePathsDTO.class);
+            PredicateOperand propertyPredicate = dobj.get("path");
+            String param = "prefix";
+            PredicateOperand propertyLimit = dobj.param(param);
+            Predicate like = propertyPredicate.like(propertyLimit).and(dobj.get("partKey").equal(dobj.param("partKeyParam")));
+            dobj.where(like);
+            Query query = session.createQuery(dobj);
+            query.setParameter(param, prefix + "%");
+            query.setParameter("partKeyParam", PART_KEY_VAL);
+            return createList(query.getResultList());
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
     }
-  }
 
-  @Override
-  public Collection<HopLeasePath> findAll() throws StorageException {
-    try {
-      Session session = connector.obtainSession();
-      QueryBuilder qb = session.getQueryBuilder();
-      QueryDomainType dobj = qb.createQueryDefinition(LeasePathsDTO.class);
-      Predicate pred = dobj.get("partKey").equal(dobj.param("param"));
-      dobj.where(pred);
-      Query query = session.createQuery(dobj);
-      query.setParameter("param", PART_KEY_VAL);
-      return createList(query.getResultList());
-    } catch (Exception e) {
-      throw new StorageException(e);
+    @Override
+    public Collection<HopLeasePath> findAll() throws StorageException {
+        try {
+            Session session = connector.obtainSession();
+            QueryBuilder qb = session.getQueryBuilder();
+            QueryDomainType dobj = qb.createQueryDefinition(LeasePathsDTO.class);
+            Predicate pred = dobj.get("partKey").equal(dobj.param("param"));
+            dobj.where(pred);
+            Query query = session.createQuery(dobj);
+            query.setParameter("param", PART_KEY_VAL);
+            return createList(query.getResultList());
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
     }
-  }
 
-  @Override
-  public void removeAll() throws StorageException {
-    try {
-      Session session = connector.obtainSession();
-      session.deletePersistentAll(LeasePathsDTO.class);
-    } catch (Exception e) {
-      throw new StorageException(e);
+    @Override
+    public void removeAll() throws StorageException {
+        try {
+            Session session = connector.obtainSession();
+            session.deletePersistentAll(LeasePathsDTO.class);
+        } catch (Exception e) {
+            throw new StorageException(e);
+        }
     }
-  }
 
-  private List<HopLeasePath> createList(Collection<LeasePathsDTO> dtos) {
-    List<HopLeasePath> list = new ArrayList<HopLeasePath>();
-    for (LeasePathsDTO leasePathsDTO : dtos) {
-      list.add(createLeasePath(leasePathsDTO));
+    private List<HopLeasePath> createList(Collection<LeasePathsDTO> dtos) {
+        List<HopLeasePath> list = new ArrayList<HopLeasePath>();
+        for (LeasePathsDTO leasePathsDTO : dtos) {
+            list.add(createLeasePath(leasePathsDTO));
+        }
+        return list;
     }
-    return list;
-  }
 
-  private HopLeasePath createLeasePath(LeasePathsDTO leasePathTable) {
-    return new HopLeasePath(leasePathTable.getPath(), leasePathTable.getHolderId());
-  }
+    private HopLeasePath createLeasePath(LeasePathsDTO leasePathTable) {
+        return new HopLeasePath(leasePathTable.getPath(), leasePathTable.getHolderId());
+    }
 
-  private void createPersistableLeasePathInstance(HopLeasePath lp, LeasePathsDTO lTable) {
-    lTable.setHolderId(lp.getHolderId());
-    lTable.setPath(lp.getPath());
-    lTable.setPartKey(PART_KEY_VAL);
-  }
+    private void createPersistableLeasePathInstance(HopLeasePath lp, LeasePathsDTO lTable) {
+        lTable.setHolderId(lp.getHolderId());
+        lTable.setPath(lp.getPath());
+        lTable.setPartKey(PART_KEY_VAL);
+    }
 }
