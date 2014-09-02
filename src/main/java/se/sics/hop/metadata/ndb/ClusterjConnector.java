f9c300435b758a9377fc180629eb1e8699493b4f
Steffen Grohsschmiedt <steffeng@sics.se>
2014-09-02 16:28:43 +0200

    Use proper error code when terminating because a transaction was started inside of another one.

--- src/main/java/se/sics/hop/metadata/ndb/ClusterjConnector.java
+++ src/main/java/se/sics/hop/metadata/ndb/ClusterjConnector.java
@@ -127,7 +127,7 @@ public class ClusterjConnector implements StorageConnector<Session> {
     Session session = obtainSession();
       if (session.currentTransaction().isActive()) {
       LOG.debug("Can not start Tx inside another Tx");
-      System.exit(0);
+      System.exit(-1);
     }
     session.currentTransaction().begin();
     } catch (Exception e) {
