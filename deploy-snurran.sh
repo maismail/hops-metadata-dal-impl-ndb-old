3d3b94be412da20b12d78f248b09fd71f7bb644c
Jim Dowling <jdowling@sics.se>
2015-02-24 14:35:37 +0100

    deploy script added

--- deploy-snurran.sh
+++ deploy-snurran.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ $# -ne 1 ] ; then
-echo "usage: <prog> version (e.g., 2.4.0)"
+  echo "usage: <prog> version (e.g., 2.4.0)"
+  exit 1
 fi
+mvn clean package
 scp target/hop-metadata-dal-impl-ndb-1.0-SNAPSHOT.jar glassfish@snurran.sics.se:/var/www/hops/ndb-dal-$1.jar
 
