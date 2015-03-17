8327680c9b0bfea310288e438e3498c9b9279b5b
Steffen Grohsschmiedt <steffeng@sics.se>
2015-03-17 11:01:38 +0100

    Remove template notes

--- src/main/java/se/sics/hop/metadata/ndb/dalimpl/yarn/AppSchedulingInfoClusterJ.java
+++ src/main/java/se/sics/hop/metadata/ndb/dalimpl/yarn/AppSchedulingInfoClusterJ.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package se.sics.hop.metadata.ndb.dalimpl.yarn;
 
 import com.mysql.clusterj.annotation.Column;
